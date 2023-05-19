1ace83cb10d73ecfe603c564f77dc87b590ab1dd
Sebastiano <sebyguzz@icloud.com>
2023-05-19 10:04:30 +0200

    Tutto dentro il container html

--- index.php
+++ index.php
@@ -9,49 +9,50 @@
 </head>
 
 <body>
+  <div class="container">
+    <h2>Generatore di password sicure</h2>
+    <form action="index.php" method="get">
+      <label for="length">Lunghezza password:</label>
+      <input type="number" id="length" name="length" min="6" max="20" required>
+      <input type="submit" value="Genera password">
+    </form>
 
-  <form action="index.php" method="get">
-    <label for="length">Lunghezza password:</label>
-    <input type="number" id="length" name="length" min="6" max="20" required>
-    <input type="submit" value="Genera password">
-  </form>
 
 
+    <?php
+    function generatePassword($length)
+    {
+      // Caratteri validi per la password
+      $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()';
 
-  <?php
-  function generatePassword($length)
-  {
-    // Caratteri validi per la password
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()';
+      $password = '';
+      $characterCount = strlen($characters);
 
-    $password = '';
-    $characterCount = strlen($characters);
+      // Genera una password casuale
+      for ($i = 0; $i < $length; $i++) {
+        $randomIndex = mt_rand(0, $characterCount - 1);
+        $password .= $characters[$randomIndex];
+      }
 
-    // Genera una password casuale
-    for ($i = 0; $i < $length; $i++) {
-      $randomIndex = mt_rand(0, $characterCount - 1);
-      $password .= $characters[$randomIndex];
+      return $password;
     }
 
-    return $password;
-  }
+    // Controlla se è stata inviata la richiesta con la lunghezza della password
+    if (isset($_GET['length'])) {
+      $length = (int)$_GET['length'];
 
-  // Controlla se è stata inviata la richiesta con la lunghezza della password
-  if (isset($_GET['length'])) {
-    $length = (int)$_GET['length'];
+      // Genera la password
+      $password = generatePassword($length);
+    ?>
 
-    // Genera la password
-    $password = generatePassword($length);
-  ?>
+    <div class="password">
+      La password generata è: <?php echo $password; ?>
+    </div>
 
-  <div class="password">
-    La password generata è: <?php echo $password; ?>
+    <?php
+    }
+    ?>
   </div>
-
-  <?php
-  }
-  ?>
-
 </body>
 
 </html>
