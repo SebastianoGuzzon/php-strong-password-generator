ebbe59e2469a55e8b205a03d90bebb9bd662f2fe
Sebastiano <sebyguzz@icloud.com>
2023-05-19 10:02:20 +0200

    Creazione del File.php

--- index.php
+++ index.php
@@ -9,6 +9,39 @@
 </head>
 
 <body>
+  <?php
+  function generatePassword($length)
+  {
+    // Caratteri validi per la password
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()';
+
+    $password = '';
+    $characterCount = strlen($characters);
+
+    // Genera una password casuale
+    for ($i = 0; $i < $length; $i++) {
+      $randomIndex = mt_rand(0, $characterCount - 1);
+      $password .= $characters[$randomIndex];
+    }
+
+    return $password;
+  }
+
+  // Controlla se è stata inviata la richiesta con la lunghezza della password
+  if (isset($_GET['length'])) {
+    $length = (int)$_GET['length'];
+
+    // Genera la password
+    $password = generatePassword($length);
+  ?>
+
+  <div class="password">
+    La password generata è: <?php echo $password; ?>
+  </div>
+
+  <?php
+  }
+  ?>
 
 </body>
 
